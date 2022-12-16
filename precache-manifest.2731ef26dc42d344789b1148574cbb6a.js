377944c536aab0b86190e0d85dfcebc8c9f5b175
Yoga1967 <adrianuseka67@gmail.com>
2022-12-16 11:53:12 +0700

    Updates

---
+++ precache-manifest.2731ef26dc42d344789b1148574cbb6a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa0d701bb0ec480e40c4",
+    "url": "/static/css/main.c80f3922.chunk.css"
+  },
+  {
+    "revision": "aa0d701bb0ec480e40c4",
+    "url": "/static/js/main.aa0d701b.chunk.js"
+  },
+  {
+    "revision": "8453a32ff8b6ffbb93e8",
+    "url": "/static/js/1.8453a32f.chunk.js"
+  },
+  {
+    "revision": "5b275d347a870a933e3b",
+    "url": "/static/js/2.5b275d34.chunk.js"
+  },
+  {
+    "revision": "91e3555aa90107fb599a",
+    "url": "/static/js/runtime~main.91e3555a.js"
+  },
+  {
+    "revision": "057ae425abd2bad25deee80e91e170d5",
+    "url": "/static/media/img01.057ae425.jpg"
+  },
+  {
+    "revision": "34751d34d3803b55310c005fddf65ac7",
+    "url": "/static/media/header-background.34751d34.jpg"
+  },
+  {
+    "revision": "e704c0963da5258526efabe45366d25a",
+    "url": "/index.html"
+  }
+];
