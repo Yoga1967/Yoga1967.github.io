c885822378aa93a89c081c59affd98afb0ba0d39
Yoga1967 <adrianuseka67@gmail.com>
2022-12-16 10:56:01 +0700

    Updates

---
+++ precache-manifest.c212abaf7081cc0f4b7601efb931c3e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f452703d05451c25238",
+    "url": "/Yoga1967/Yoga1967.github.io/static/css/main.54ee3402.chunk.css"
+  },
+  {
+    "revision": "6f452703d05451c25238",
+    "url": "/Yoga1967/Yoga1967.github.io/static/js/main.6f452703.chunk.js"
+  },
+  {
+    "revision": "1b3f48c6de62d66542ce",
+    "url": "/Yoga1967/Yoga1967.github.io/static/js/1.1b3f48c6.chunk.js"
+  },
+  {
+    "revision": "d609a27c904bc3243e67",
+    "url": "/Yoga1967/Yoga1967.github.io/static/js/2.d609a27c.chunk.js"
+  },
+  {
+    "revision": "105407bd78eef547e132",
+    "url": "/Yoga1967/Yoga1967.github.io/static/js/runtime~main.105407bd.js"
+  },
+  {
+    "revision": "057ae425abd2bad25deee80e91e170d5",
+    "url": "/Yoga1967/Yoga1967.github.io/static/media/img01.057ae425.jpg"
+  },
+  {
+    "revision": "34751d34d3803b55310c005fddf65ac7",
+    "url": "/Yoga1967/Yoga1967.github.io/static/media/header-background.34751d34.jpg"
+  },
+  {
+    "revision": "c475b5c8a94345920b3970d8b977f873",
+    "url": "/Yoga1967/Yoga1967.github.io/index.html"
+  }
+];
