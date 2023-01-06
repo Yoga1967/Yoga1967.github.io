4e5c8e69fa8ce8e81936f83938723fb9c9c0ad3b
Yoga1967 <adrianuseka67@gmail.com>
2023-01-07 00:07:43 +0700

    add smooth scroll

--- simple-resume/src/Components/Header.js
+++ simple-resume/src/Components/Header.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 
+
 class Header extends Component{
     render(){
         return (
             <div className="header position-sticky z-index-10">
                 <div class="background"></div>
                 <div className="d-flex j-c-center flex-c-gap-30 f-w-700 foreground f-c-F97316 f-c-hover-fff">
-                    <p><a href="#about">about</a></p>
-                    <p><a href="#education">education</a></p>
-                    <p><a href="#work">work</a></p>
+                    <p><a href="#about" class="scroll">about</a></p>
+                    <p><a href="#education" class="scroll">education</a></p>
+                    <p><a href="#work" class="scroll">work</a></p>
                     {/*p><a href="#contact">contact</a></p>*/}
                 </div>
             </div>
