557a00aa9d0e35c774aff3e0bbe893dbef2d1049
Yoga1967 <adrianuseka67@gmail.com>
2023-02-21 01:56:40 +0700

    Updates

---
+++ precache-manifest.a23e1f059d67cb2034a5031c6044c0af.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa4167a1608568133123",
+    "url": "/static/css/main.9aa5ce6a.chunk.css"
+  },
+  {
+    "revision": "fa4167a1608568133123",
+    "url": "/static/js/main.fa4167a1.chunk.js"
+  },
+  {
+    "revision": "093ecf0bec58628b4322",
+    "url": "/static/js/1.093ecf0b.chunk.js"
+  },
+  {
+    "revision": "d1d0cefc2a901fb6bdbb",
+    "url": "/static/css/2.295967bc.chunk.css"
+  },
+  {
+    "revision": "d1d0cefc2a901fb6bdbb",
+    "url": "/static/js/2.d1d0cefc.chunk.js"
+  },
+  {
+    "revision": "18659a56ac87fef7159a",
+    "url": "/static/js/runtime~main.18659a56.js"
+  },
+  {
+    "revision": "057ae425abd2bad25deee80e91e170d5",
+    "url": "/static/media/img01.057ae425.jpg"
+  },
+  {
+    "revision": "013d56050450484dc878b1363cb6f60b",
+    "url": "/static/media/header-background.013d5605.jpg"
+  },
+  {
+    "revision": "8510d8e0870c47dd04c25c7d924e90dc",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dfb877fb529f064d08404719ec4f2211.js"
+  "/precache-manifest.a23e1f059d67cb2034a5031c6044c0af.js"
 );
 
 workbox.clientsClaim();
