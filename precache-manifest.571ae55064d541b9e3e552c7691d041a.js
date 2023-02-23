382d561e6ce08de94ccd95f0e5198528ff6b51b7
Yoga1967 <adrianuseka67@gmail.com>
2023-02-23 08:22:47 +0700

    Updates

---
+++ precache-manifest.571ae55064d541b9e3e552c7691d041a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "15281e413180938481f1",
+    "url": "/static/css/main.2e6b51a9.chunk.css"
+  },
+  {
+    "revision": "15281e413180938481f1",
+    "url": "/static/js/main.15281e41.chunk.js"
+  },
+  {
+    "revision": "093ecf0bec58628b4322",
+    "url": "/static/js/1.093ecf0b.chunk.js"
+  },
+  {
+    "revision": "c3c22b877db1565f6463",
+    "url": "/static/css/2.295967bc.chunk.css"
+  },
+  {
+    "revision": "c3c22b877db1565f6463",
+    "url": "/static/js/2.c3c22b87.chunk.js"
+  },
+  {
+    "revision": "18659a56ac87fef7159a",
+    "url": "/static/js/runtime~main.18659a56.js"
+  },
+  {
+    "revision": "057ae425abd2bad25deee80e91e170d5",
+    "url": "/static/media/img01.057ae425.jpg"
+  },
+  {
+    "revision": "013d56050450484dc878b1363cb6f60b",
+    "url": "/static/media/header-background.013d5605.jpg"
+  },
+  {
+    "revision": "fea93c2a330ef4020321b6b2d5bca2a3",
+    "url": "/index.html"
+  }
+];
